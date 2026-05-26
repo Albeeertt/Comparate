179894d329d337da398127b8747739d530b28f8f
Albeeertt <alberto.sanchezsoto16@gmail.com>
2026-05-26 12:33:49 +0200

    add TE comparate

---
+++ comparate/core/Comparative_TE.py
@@ -0,0 +1,116 @@
+
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+
+from typing import Dict, List
+
+class Comparative_TE: 
+
+    def __init__(self,  route_truth: str, te_route_truth: str):
+        self.name_truth = route_truth.split('/')[-1]
+        self.route_truth = route_truth
+        self.route_te = te_route_truth
+
+        self.truth_file_read = self.obtain_csv(route_truth)
+        self.te_file_read = self.obtain_gff(te_route_truth)
+
+    def obtain_gff(self, route: str, encoding: str = 'utf-8') -> pd.DataFrame:
+
+        data = pd.read_csv(route, comment='#', sep='\t', header=None, encoding=encoding)
+        data.columns = ['chr',	'db',	'type',	'start',	'end',	'score',	'strand',	'phase',	'attributes']
+        data['old_idx'] = data.index
+        data['strand'] = data['strand'].replace('.', '+')
+        return data
+
+
+    def obtain_csv(self, route: str, encoding: str = 'utf-8') -> pd.DataFrame:
+        '''
+        - Read the GFF3 located in 'route' with the encoding specified in 'encoding'. 
+          Additionally, add the old_idx column, indicating the location of each sample 
+          in the original dataframe (for future modifications).
+        '''
+        data = pd.read_csv(route, index_col=0, encoding= encoding)
+        data['old_idx'] = data.index
+        data['strand'] = data['strand'].replace('.', '+')
+        return data
+    
+    def obtain_structure_TE(self, value_kimura: float = 1.) -> Dict:
+        structure_te = defaultdict(defaultdict(list))
+        for record in self.te_file_read.to_dict(orient='records'):
+            dict_attributes = { part.split('=')[0]:part.split('=')[1] for part in record['attributes'].split(';')}
+            if dict_attributes.get('KIMURA80', -1) != -1:
+                record['KIMURA80'] = float(dict_attributes['KIMURA80'])
+                if record['KIMURA80'] <= value_kimura:
+                    structure_te[record['chr']][record['strand']].append(record)
+            else:
+                structure_te[record['chr']][record['strand']].append(record)
+        return structure_te
+    
+    def comparate_te(self, threshold: float = .5):
+        result_ir_te_unicos = defaultdict(lambda: {
+            f'prob_mayor_{threshold}': 0,
+            f'prob_menor_{threshold}': 0
+        })
+       
+        result_gen_te_unicos = defaultdict(lambda: {
+            f'prob_mayor_{threshold}': 0,
+            f'prob_menor_{threshold}': 0
+        }) 
+    
+        for record in self.truth_file_read.to_dict(orient='records'):
+            if pd.isna(record['prob_gene']):
+                continue
+
+            tipo_te_lista = []
+            structure_te = self.obtain_structure_TE()
+
+            list_records_chr: List[Dict] = structure_te[record['chr']][record['strand']]
+            posee_te: bool = False
+            for record_te in list_records_chr:
+                # if record['end'] > record_te['start'] and record_te['end'] > record['start']:
+                if record['start'] <= record_te['start'] and record['end'] >= record_te['end']:
+                    posee_te = True
+                    tipo_te_lista.append(record_te['type'])
+
+            tipo_te_lista = np.unique(tipo_te_lista)
+                    
+            if posee_te and record['type'] == 'gene':
+                if record['prob_gene'] >= threshold:
+                    for tipoo in tipo_te_lista:
+                        result_gen_te_unicos[tipoo][f'prob_mayor_{threshold}'] += 1
+                else:
+                    for tipoo in tipo_te_lista:
+                        result_gen_te_unicos[tipoo][f'prob_menor_{threshold}'] += 1
+            elif posee_te and record['type'] == 'intergenic_region':
+                if record['prob_gene'] >= threshold:
+                    for tipoo in tipo_te_lista:
+                        result_ir_te_unicos[tipoo][f'prob_mayor_{threshold}'] += 1            
+                else:
+                    for tipoo in tipo_te_lista:
+                        result_ir_te_unicos[tipoo][f'prob_menor_{threshold}'] += 1      
+
+        return result_ir_te_unicos, result_gen_te_unicos
+
+    def generate_graph(self, results_to_graph: Dict, y_label: str, title: str, out: str):
+        labels = list(results_to_graph.keys())
+        mayor = [results_to_graph[k]['prob_mayor_0.5'] for k in labels]
+        menor = [results_to_graph[k]['prob_menor_0.5'] for k in labels]
+
+        x = np.arange(len(labels))
+        width = 0.4
+        
+        plt.figure(figsize=(12,6))
+        plt.bar(x - width/2, mayor, width, label='prob > 0.5')
+        plt.bar(x + width/2, menor, width, label='prob < 0.5')
+        plt.xticks(x, labels, rotation=45, ha='right')
+        plt.ylabel(y_label)
+        plt.title(title)
+        plt.legend()
+
+        plt.tight_layout()
+
+        plt.savefig(out, dpi=300, bbox_inches='tight')
+
--- comparate/main.py
+++ comparate/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from comparate.core.HandleFile import HandleFile
 from comparate.core.Comparative import Comparate
+from comparate.core.Comparative_TE import Comparative_TE
 
 
 def obtain_arguments():
@@ -12,9 +13,11 @@ def obtain_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--route_csv_truth', required=True, type=str, help='Path to CSV file.')
-    parser.add_argument('--route_csv_other', required=True, type=str, help='Path to CSV file.')
     parser.add_argument('--out', type=str, required=True, help='Output path.')
-    parser.add_argument('--route_te', type=str, help='Path to GFF file.')
+    parser.add_argument('--route_csv_other', type=str, help='Path to CSV file.')
+    parser.add_argument('--route_TE_truth', type=str, help='Path to TE (truth CSV) in GFF file.')
+    parser.add_argument('--route_TE_other', type=str, help='Path to TE (other CSV) in GFF file.')
+    parser.add_argument('--compare', action='store_true', help='compare route_csv_truth and route_csv_other.')
     parser.add_argument('--threshold_complete_match', type=float, default=.5, help='threshold_complete_match')
     parser.add_argument('--threshold_overlap_match', type=float, default=.2, help='threshold_overlap_match')
     return parser.parse_args()
@@ -31,28 +34,42 @@ def execute_main_program():
     ROUTE_SUMMARY_TRUTH = route_out+'summary_truth.csv'
     ROUTE_SUMMARY_OTHER = route_out+'summary_other.csv'
     ROUTE_COMPARATE = route_out+'comparate.csv'
+    ROUTE_GRAPH_TE_IR = route_out+'te_ir_truth.png'
+    ROUTE_GRAPH_TE_GEN = route_out+'te_gen_truth.png'
+    ROUTE_GRAPH_TE_IR_OTHER = route_out+'te_ir_other.png'
+    ROUTE_GRAPH_TE_GEN_OTHER = route_out+'te_gen_other.png'
         
     instance_HandleFile_truth = HandleFile(args.route_csv_truth)
-    instance_HandleFile_other = HandleFile(args.route_csv_other)
-    instance_comparate = Comparate(args.route_csv_truth, args.route_csv_other, threshold_complete_match=args.threshold_complete_match, threshold_overlap_match=args.threshold_overlap_match)
-
     summary_truth = []
     summary_truth.append(instance_HandleFile_truth.summary_file())
     summary_truth.extend(instance_HandleFile_truth.summary_chr())
-
-    summary_other = []
-    summary_other.append(instance_HandleFile_other.summary_file())
-    summary_other.extend(instance_HandleFile_other.summary_chr())
-
     pd.DataFrame(summary_truth).to_csv(ROUTE_SUMMARY_TRUTH)
-    pd.DataFrame(summary_other).to_csv(ROUTE_SUMMARY_OTHER)
 
-    comparate_files = []
-    complete, overlap = instance_comparate.comparate_files()
-    comparate_files.append(complete)
-    comparate_files.append(overlap)
+    if args.route_csv_other:
+        instance_HandleFile_other = HandleFile(args.route_csv_other)
+        summary_other = []
+        summary_other.append(instance_HandleFile_other.summary_file())
+        summary_other.extend(instance_HandleFile_other.summary_chr())
+        pd.DataFrame(summary_other).to_csv(ROUTE_SUMMARY_OTHER)
 
-    comparate_files.extend(instance_comparate.comparate_chrs())
+    if args.route_csv_other and args.compare:
+        instance_comparate = Comparate(args.route_csv_truth, args.route_csv_other, threshold_complete_match=args.threshold_complete_match, threshold_overlap_match=args.threshold_overlap_match)
+        comparate_files = []
+        complete, overlap = instance_comparate.comparate_files()
+        comparate_files.append(complete)
+        comparate_files.append(overlap)
 
-    pd.DataFrame(comparate_files).to_csv(ROUTE_COMPARATE)
+        comparate_files.extend(instance_comparate.comparate_chrs())
 
+        pd.DataFrame(comparate_files).to_csv(ROUTE_COMPARATE)
+
+    if args.route_TE_truth:
+        instance_comparate_TE = Comparative_TE(args.route_csv_truth, args.route_TE_truth)
+        result_ir_te_unicos, result_gen_te_unicos = instance_comparate_TE.comparate_te()
+        instance_comparate_TE.generate_graph(result_ir_te_unicos, "Conteo de los TE en regiones intergénicas clasificados como gen", f"Elementos transponibles en regiones intergénicas ({instance_comparate_TE.name_truth})", ROUTE_GRAPH_TE_IR)
+        instance_comparate_TE.generate_graph(result_gen_te_unicos, "Conteo de los TE en genes clasificados como gen", f"Elementos transponibles en genes ({instance_comparate_TE.name_truth})", ROUTE_GRAPH_TE_GEN)
+    if args.route_csv_other and args.route_TE_other:
+        instance_comparate_TE = Comparative_TE(args.route_csv_other, args.route_TE_other)
+        result_ir_te_unicos, result_gen_te_unicos = instance_comparate_TE.comparate_te()
+        instance_comparate_TE.generate_graph(result_ir_te_unicos, "Conteo de los TE en regiones intergénicas clasificados como gen", f"Elementos transponibles en regiones intergénicas ({instance_comparate_TE.name_truth})", ROUTE_GRAPH_TE_IR_OTHER)
+        instance_comparate_TE.generate_graph(result_gen_te_unicos, "Conteo de los TE en genes clasificados como gen", f"Elementos transponibles en genes ({instance_comparate_TE.name_truth})", ROUTE_GRAPH_TE_GEN_OTHER)
